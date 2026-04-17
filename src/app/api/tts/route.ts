59ff6dd2eff1976a7c9bcc65ebaf56a597267660
FloatinggOnion <jesseosems123@gmail.com>
2026-04-17 17:57:23 +0100

    update language code

--- src/app/api/tts/route.ts
+++ src/app/api/tts/route.ts
@@ -38,7 +38,7 @@ export async function POST(request: NextRequest) {
     const [response] = await client.synthesizeSpeech({
       input: { text },
       voice: {
-        languageCode: 'en-NG',
+        languageCode: 'yo-NG',
         name: voiceName,
         ssmlGender: gender === 'male' ? 'MALE' : 'FEMALE'
       },
